c8b634b22fe178e5d812ea9243d89defd38a7f0d
iOSXiaoQiao <2996861140@qq.com>
2017-02-27 17:00:07 +0800

    创建项目并导入资源文件
    创建打砖块项目并导入资源

---
+++ XZPlayBall/XZPlayBall/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XZPlayBall
+//
+//  Created by xiyang on 2017/2/27.
+//  Copyright © 2017年 xiyang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    
+    
+}
+
